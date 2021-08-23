61ad4b99327c6de9208bd3babd25a50c46977610
Nataf Cohen <nataf1231@gmail.com>
2021-08-23 15:12:52 +0300

    added edit

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const path = require("path")
 const mongoose = require('mongoose');
+const methodOverride = require("method-override")
 const Campground = require('./models/compground')
 
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", 'ejs')
+
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',
     {
@@ -29,8 +33,33 @@ app.get('/', (req, res) => {
     res.render("home.ejs")
 });
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: "myCamp" })
-    await camp.save()
-    res.send(camp)
+app.get('/campgrounds', async (req, res) => {
+    const camps = await Campground.find()
+    res.render("campgrounds/index.ejs", {camps})
 });
+
+app.get('/campgrounds/new', (req, res) => {
+
+    res.render("campgrounds/new.ejs")
+});
+
+app.post('/campgrounds', async(req, res) => {
+    const camp = new Campground(req.body.campground)
+    await camp.save();
+    res.redirect(`/campgrounds/${camp._id}`)
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const camp = await Campground.findById(req.params.id)
+    res.render("campgrounds/show.ejs", {camp})
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const camp = await Campground.findById(req.params.id)
+    res.render("campgrounds/edit.ejs", {camp})
+});
+
+app.put('/campgrounds/:id', async(req, res) => {
+    const camp = await Campground.findByIdAndUpdate(req.params.id, {...req.body.campground});
+    res.redirect(`/campgrounds/${camp._id}`);
+})
