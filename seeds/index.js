603fa6c7ed4b41cb466d740662a081df30dfb1da
nataf cohen <nataf1231@gmail.com>
2021-08-24 12:35:53 +0300

    added styles

--- seeds/index.js
+++ seeds/index.js
@@ -14,7 +14,7 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp',
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"))
 db.once("open", () => {
-    console.log("Database connected")
+    console.log("Database connected in reset")
 })
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
@@ -24,9 +24,13 @@ const seedDB = async () => {
     for(let i = 0; i < 50; i++)
     {
         const rand = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[rand].city}, ${cities[rand].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image : 'https://source.unsplash.com/collection/483251',
+            description : 'a very nice place, a very nice place, a very nice place, a very nice place, a very nice place and a very nice place',
+            price: price,
         })
         await camp.save();
     }
